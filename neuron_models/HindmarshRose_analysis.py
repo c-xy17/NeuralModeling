b98731b8d6d0d651a207fdea83256b3727a97162
c-xy17 <c-xy17@tsinghua.org.cn>
2022-01-13 10:01:54 +0800

    modify HindmarshRose

--- neuron_models/HindmarshRose_analysis.py
+++ neuron_models/HindmarshRose_analysis.py
@@ -54,20 +54,31 @@ class HindmarshRose(bp.NeuGroup):
 		self.input[:] = 0.  # 重置外界输入
 
 
-# 相平面分析
 group = HindmarshRose(1)
+
+# 定义分析器
 phase_plane_analyzer = bp.analysis.PhasePlane2D(
 	model=[group.int_V, group.int_y, group.int_z],
-	target_vars={'V': [-3., 3.], 'y': [-20., 5.]},
-	fixed_vars={'z': 1.4},
-	pars_update={'Iext': 2.},
+	target_vars={'V': [-3., 3.], 'y': [-20., 5.]},  # 待分析变量
+	fixed_vars={'z': 1.4},                          # 固定变量
+	pars_update={'Iext': 2.},                       # 需要更新的变量
 	resolutions=0.01
 )
-phase_plane_analyzer.plot_nullcline(x_style={'fmt': '-'}, y_style={'fmt': '-'})
+
+# 画出V, y的零增长曲线
+phase_plane_analyzer.plot_nullcline(x_style=dict(color='cornflowerblue', fmt='--', linewidth=2, alpha=0.7),
+                                    y_style=dict(color='lightcoral', fmt='--', linewidth=2, alpha=0.7))
+
+# 画出固定点
 phase_plane_analyzer.plot_fixed_point()
-phase_plane_analyzer.plot_vector_field()
+
+# 画出向量场
+phase_plane_analyzer.plot_vector_field(plot_style=dict(color='lightgrey'))
+
+# 画出V, y的变化轨迹
 phase_plane_analyzer.plot_trajectory(
 	{'V': [1.], 'y': [0.], 'z': [1.4]},
 	duration=100.,
+	color='darkslateblue', linewidth=1.5, alpha=0.9,
 	show=True
 )
