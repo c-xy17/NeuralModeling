cc3d7534c67ddd6e09bcd39372e3d97aaff22975
c-xy17 <c-xy17@tsinghua.org.cn>
2022-03-04 21:31:44 +0800

    modifications of AdEx

--- model_analysis/AdEx_patterns.py
+++ model_analysis/AdEx_patterns.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 
 class AdEx(bp.dyn.NeuGroup):
-  def __init__(self, size, V_rest=-65., V_reset=-68., V_th=20., V_T=-60., delta_T=1., a=1.,
-               b=2.5, R=1., tau=10., tau_w=30., tau_ref=2., **kwargs):
+  def __init__(self, size, V_rest=-70., V_reset=-65., V_th=0., V_T=-50., delta_T=2., a=1.,
+               b=2.5, R=0.5, tau=10., tau_w=30., tau_ref=2., **kwargs):
     # 初始化父类
     super(AdEx, self).__init__(size=size, **kwargs)
 
@@ -23,7 +23,7 @@ class AdEx(bp.dyn.NeuGroup):
     self.R = R
 
     # 初始化变量
-    self.V = bm.Variable(bm.random.randn(self.num) + V_reset)
+    self.V = bm.Variable(bm.zeros(self.num) + V_reset)
     self.w = bm.Variable(bm.zeros(self.num))
     self.input = bm.Variable(bm.zeros(self.num))
     self.t_last_spike = bm.Variable(bm.ones(self.num) * -1e7)  # 上一次脉冲发放时间
@@ -61,33 +61,40 @@ class AdEx(bp.dyn.NeuGroup):
     self.input[:] = 0.  # 重置外界输入
 
 
-# # 运行AdExIF模型
-# neu = AdEx(1)
-# runner = bp.StructRunner(neu, monitors=['V', 'w'], inputs=('input', 9.), dt=0.01)
-# runner(500)
-#
-# # 可视化V和w的变化
-# plt.plot(runner.mon.ts, runner.mon.V, label='V')
-# plt.plot(runner.mon.ts, runner.mon.w, label='w')
-# plt.xlabel('t (ms)')
-# plt.ylabel('V (mV)')
-#
-# plt.show()
-#
-# # fig, ax1 = plt.subplots()
-# # ax1.plot(runner.mon.ts, runner.mon.V[:, 0], label='V')
-# # ax1.set_ylabel('V')
-# #
-# # ax2 = ax1.twinx()
-# # ax2.plot(runner.mon.ts, runner.mon.w[:, 0], color='orange', label='w')
-# # ax2.set_ylabel('w')
-# #
-# # fig.legend(loc="upper center")
-# # plt.show()
-#
-#
-# # fig, ax1 = plt.subplots()
-# # bp.visualize.line_plot(runner.mon.ts, runner.mon.w, ax=ax1, ylabel='w', legend='w', show=False)
-# #
-# # ax2 = ax1.twinx()
-# # bp.visualize.line_plot(runner.mon.ts, runner.mon.V, ax=ax2, ylabel='V', legend='V', show=True)
+def subplot(i, neu, title=None, input=('input', 65.), duration=400):
+  runner = bp.StructRunner(neu, monitors=['V', 'w'], inputs=input)
+  runner(duration)
+
+  ax1 = plt.subplot(2, 3, i)
+  ax1.plot(runner.mon.ts, runner.mon.V, label='V')
+  ax1.set_ylim(- 65, 15)
+  ax1.set_xlabel('t (ms)')
+  ax1.set_xlim(0, 400)
+  if i % 3 == 1:
+    ax1.set_ylabel('V')
+
+  ax2 = ax1.twinx()
+  ax2.plot(runner.mon.ts, runner.mon.w, label='w', color=u'#ff7f0e')
+  w_min, w_max = min(runner.mon.w), max(runner.mon.w)
+  ax2.set_ylim(w_max - (w_max - w_min) * 3, w_max + 5)
+  if i % 3 == 0:
+    ax2.set_ylabel('w')
+
+  handles1, labels1 = ax1.get_legend_handles_labels()
+  handles2, labels2 = ax2.get_legend_handles_labels()
+  plt.legend(handles1 + handles2, labels1 + labels2, loc='upper right')
+  plt.title(title)
+
+
+plt.figure(figsize=(12, 6))
+
+subplot(1, AdEx(1, tau=20., a=0., tau_w=30., b=60., V_reset=-55.), title='Tonic Spiking')
+subplot(2, AdEx(1, tau=20., a=0., tau_w=100., b=5., V_reset=-55.), title='Adaptation')
+subplot(3, AdEx(1, tau=5., a=0.5, tau_w=100., b=7., V_reset=-51.), title='Initial Bursting')
+subplot(4, AdEx(1, tau=5., a=-0.5, tau_w=100., b=7., V_reset=-47.), title='Bursting')
+subplot(5, AdEx(1, tau=10., a=1., tau_w=100., b=10., V_reset=-60.), title='Transient Spiking', input=('input', 55.))
+subplot(6, AdEx(1, tau=5., a=-1., tau_w=100., b=5., V_reset=-60.), title='Delayed Spiking', input=('input', 25.))
+# subplot(7, AdEx(1, tau=9.9, a=-0.5, tau_w=100., b=7., V_reset=-46.), title='irregular')
+
+plt.tight_layout()
+plt.show()
