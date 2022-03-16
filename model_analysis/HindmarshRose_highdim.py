4e51197a4653a76048f2902df286e8b911a28729
c-xy17 <c-xy17@tsinghua.org.cn>
2022-03-16 10:09:57 +0800

    HR high-dim

--- model_analysis/HindmarshRose_highdim.py
+++ model_analysis/HindmarshRose_highdim.py
@@ -9,13 +9,20 @@ bp.math.enable_x64()
 
 model = HindmarshRose(1)
 
-finder = bp.analysis.SlowPointFinder(f_cell=model.derivative)
+
+def f_cell(x):
+  x, y, z = x
+  res = model.derivative(x, y, z, 0, 2.)
+  return bm.asarray(res)
+
+
+finder = bp.analysis.SlowPointFinder(f_cell=f_cell)
 finder.find_fps_with_gd_method(
-    candidates=bm.random.random((1000, 2)), tolerance=1e-5, num_batch=200,
-    opt_setting=dict(method=bm.optimizers.Adam,
-                     lr=bm.optimizers.ExponentialDecay(0.01, 1, 0.9999)),
+    candidates=bm.random.random((1000, 3)), tolerance=1e-5, num_batch=200,
+    opt_setting=dict(method=bp.optim.Adam,
+                     lr=bp.optim.ExponentialDecay(0.01, 1, 0.9999)),
 )
 finder.filter_loss(1e-5)
 finder.keep_unique()
 
-finder.fixed_points()
+print(finder.fixed_points)
