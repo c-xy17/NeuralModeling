ae073da90aaef7576a0b735a14a81fac3f932e90
c-xy17 <c-xy17@tsinghua.org.cn>
2022-03-16 17:15:21 +0800

    HR bif

--- model_analysis/HindmarshRose_bif.py
+++ model_analysis/HindmarshRose_bif.py
@@ -8,9 +8,9 @@ model = HindmarshRose(1)
 
 bif = bp.analysis.Bifurcation2D(
     model=model,
-    target_vars={'x': [-2, 2], 'y': [-10, 5]},
+    target_vars={'x': [-2, 2], 'y': [-20, 5]},
     fixed_vars={'z': 1.8},
-    target_pars={'Iext': [0., 1.5]},
+    target_pars={'Iext': [0., 2.5]},
     resolutions={'Iext': 0.01}
 )
 bif.plot_bifurcation(show=True)
