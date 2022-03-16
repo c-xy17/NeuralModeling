4478141161bb080deb155cd5e50257b6994de8e1
c-xy17 <c-xy17@tsinghua.org.cn>
2022-03-16 10:09:47 +0800

    HR bif

---
+++ model_analysis/HindmarshRose_bif.py
@@ -0,0 +1,16 @@
+import brainpy as bp
+
+from neuron_models.HindmarshRose_model import HindmarshRose
+
+bp.math.enable_x64()
+
+model = HindmarshRose(1)
+
+bif = bp.analysis.Bifurcation2D(
+    model=model,
+    target_vars={'x': [-2, 2], 'y': [-10, 5]},
+    fixed_vars={'z': 1.8},
+    target_pars={'Iext': [0., 1.5]},
+    resolutions={'Iext': 0.01}
+)
+bif.plot_bifurcation(show=True)
