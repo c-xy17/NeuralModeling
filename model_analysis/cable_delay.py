5a519b976ad50e2095b697ad740bdc7c67ad19d2
c-xy17 <c-xy17@tsinghua.org.cn>
2022-03-25 09:40:02 +0800

    add cable_delay.py

--- model_analysis/cable_delay.py
+++ model_analysis/cable_delay.py
@@ -2,17 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-lamb, r_L, a, I0 = 5.4e-3, 30e6, 0.5e-3, 10
+def exp(x, lamb=1., a=1.):
+  return a * np.exp(- x / lamb)
 
-def V_ss(x):
-  return (lamb * r_L) / (np.pi * a * a) * I0 * np.exp(- x / lamb)
 
-res = 200
-x = np.linspace(0., 0.1, res)
-V = V_ss(x)
+def plot_lines(x):
+  y = exp(x)
+  plt.plot([x, x], [0, y], '--', color='grey')
+  plt.plot([0, x], [y, y], '--', color='grey')
+
+
+res = 300
+x = np.linspace(0., 5, res)
+V = exp(x)
 
 plt.plot(x, V)
+
+plot_lines(1.)
+plot_lines(2.)
+plot_lines(3.)
+
 plt.xlabel('$x$')
 plt.ylabel('$V_{ss}$')
+plt.xlim(np.min(x), np.max(x))
+plt.ylim(np.min(V), np.max(V))
+plt.xticks([])
+plt.yticks([])
 
 plt.show()
