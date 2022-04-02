c65572a492e1e6a21e98dbf7c4b3f5ac1d6ae5d3
c-xy17 <c-xy17@tsinghua.org.cn>
2022-04-02 13:18:41 +0800

    update the exponential synapse model

--- synapes_models/exponential.py
+++ synapes_models/exponential.py
@@ -6,7 +6,7 @@ from run_synapse import run_syn
 
 class Exponential(bp.dyn.TwoEndConn):
 	def __init__(self, pre, post, conn, g_max=1., tau=8.0, E=None, syn_type='CUBA', method='exp_auto',
-	             delay_step=None, **kwargs):
+	             delay=2, **kwargs):
 		super(Exponential, self).__init__(pre=pre, post=post, conn=conn, **kwargs)
 		self.check_pre_attrs('spike')
 		self.check_post_attrs('input', 'V')
@@ -25,8 +25,9 @@ class Exponential(bp.dyn.TwoEndConn):
 
 		# 初始化变量
 		self.g = bm.Variable(bm.zeros(self.post.num))
-		# 将突出前神经元传来的信号延迟delay_step的长度
-		self.delay_type, self.delay_step, self.pre_spike = bp.dyn.utils.init_delay(delay_step, self.pre.spike)
+		# 将突触前神经元传来的信号延迟delay的时长
+		self.delay = bm.LengthDelay(self.pre.spike, delay)
+		self.delay_step = delay
 
 		# 定义积分函数
 		self.integral = bp.odeint(self.derivative, method=method)
@@ -37,24 +38,20 @@ class Exponential(bp.dyn.TwoEndConn):
 
 	def update(self, _t, _dt):
 		# 处理delay
-		if self.delay_type == 'homo':
-			delayed_pre_spike = self.pre_spike(self.delay_step)
-			self.pre_spike.update(self.pre.spike)
-		elif self.delay_type == 'heter':
-			delayed_pre_spike = self.pre_spike(self.delay_step, bm.arange())
-			self.pre_spike.update(self.pre.spike)
-		else:
-			delayed_pre_spike = self.pre.spike
+		delayed_pre_spike = self.delay(self.delay_step)
+		self.delay.update(self.pre.spike)
 
 		# 根据连接模式计算各个突触后神经元收到的信号强度
 		post_sp = bm.pre2post_event_sum(delayed_pre_spike, self.pre2post, self.post.num, self.g_max)
 		# 突触的电导g的更新包括常规积分和突触前脉冲带来的跃变
-		self.g.value = self.integral(self.g.value, _t, dt=_dt) + post_sp
+		self.g.value = self.integral(self.g, _t, dt=_dt) + post_sp
 
+		# 根据不同模式计算突触后电流
 		if self.type == 'CUBA':
 			self.post.input += self.g
 		else:
 			self.post.input += self.g * (self.E - self.post.V)
 
 
-run_syn(Exponential)
+# run_syn(Exponential, syn_type='CUBA')
+run_syn(Exponential, syn_type='COBA', E=0.)
--- synapes_models/run_synapse.py
+++ synapes_models/run_synapse.py
@@ -3,14 +3,39 @@ import matplotlib.pyplot as plt
 
 
 def run_syn(syn_model, run_duration=30., **kwargs):
+	# 定义突触前神经元、突触后神经元和突触连接
 	pre_neu = bp.dyn.LIF(5)
 	post_neu = bp.dyn.LIF(3)
-
 	syn = syn_model(pre_neu, post_neu, conn=bp.conn.All2All(), **kwargs)
 
+	# 构建网络并运行模拟
 	net = bp.dyn.Network(pre=pre_neu, syn=syn, post=post_neu)
 	runner = bp.DSRunner(net, monitors=['pre.V', 'syn.g', 'post.V'], inputs=('pre.input', 35.))
 	runner(run_duration)
 
-	plt.plot(runner.mon.ts, runner.mon['syn.g'])
+	# 只选取第0个突触后神经元可视化
+	plt.plot(runner.mon.ts, runner.mon['syn.g'][:, 0])
+	plt.xlabel('t (ms)')
+	plt.ylabel('g')
 	plt.show()
+
+
+
+# neu1 = bp.dyn.HH(1)
+# neu2 = bp.dyn.HH(1)
+# syn1 = bp.dyn.ExpCOBA(neu1, neu2, bp.connect.All2All(), E=0.)
+# net = bp.dyn.Network(pre=neu1, syn=syn1, post=neu2)
+#
+# runner = bp.dyn.DSRunner(net, inputs=[('pre.input', 5.)], monitors=['pre.V', 'post.V', 'syn.g'])
+# runner.run(150.)
+#
+# fig, gs = bp.visualize.get_figure(2, 1, 3, 8)
+# fig.add_subplot(gs[0, 0])
+# plt.plot(runner.mon.ts, runner.mon['pre.V'], label='pre-V')
+# plt.plot(runner.mon.ts, runner.mon['post.V'], label='post-V')
+# plt.legend()
+#
+# fig.add_subplot(gs[1, 0])
+# plt.plot(runner.mon.ts, runner.mon['syn.g'], label='g')
+# plt.legend()
+# plt.show()
