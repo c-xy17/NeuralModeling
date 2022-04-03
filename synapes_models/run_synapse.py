15cef2bf93f32b746b31d6c2f2081a372ef2db7a
c-xy17 <c-xy17@tsinghua.org.cn>
2022-04-03 13:21:51 +0800

    Update run_synapse.py

--- synapes_models/run_synapse.py
+++ synapes_models/run_synapse.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 
 
 def run_syn(syn_model, run_duration=30., **kwargs):
-	# 定义突触前神经元、突触后神经元和突触连接
+	# 定义突触前神经元、突触后神经元和突触连接，并构建网络
 	pre_neu = bp.dyn.LIF(5)
 	post_neu = bp.dyn.LIF(3)
 	syn = syn_model(pre_neu, post_neu, conn=bp.conn.All2All(), **kwargs)
-
-	# 构建网络并运行模拟
 	net = bp.dyn.Network(pre=pre_neu, syn=syn, post=post_neu)
+
+	# 运行模拟
 	runner = bp.DSRunner(net, monitors=['pre.V', 'syn.g', 'post.V'], inputs=('pre.input', 35.))
 	runner(run_duration)
 
@@ -20,22 +20,24 @@ def run_syn(syn_model, run_duration=30., **kwargs):
 	plt.show()
 
 
+def run_syn2(syn_model, run_duration=100., **kwargs):
+	# 定义突触前神经元、突触后神经元和突触连接，并构建神经网络
+	neu1 = bp.dyn.HH(1)
+	neu2 = bp.dyn.HH(1)
+	syn1 = syn_model(neu1, neu2, conn=bp.connect.All2All(), **kwargs)
+	net = bp.dyn.Network(pre=neu1, syn=syn1, post=neu2)
 
-# neu1 = bp.dyn.HH(1)
-# neu2 = bp.dyn.HH(1)
-# syn1 = bp.dyn.ExpCOBA(neu1, neu2, bp.connect.All2All(), E=0.)
-# net = bp.dyn.Network(pre=neu1, syn=syn1, post=neu2)
-#
-# runner = bp.dyn.DSRunner(net, inputs=[('pre.input', 5.)], monitors=['pre.V', 'post.V', 'syn.g'])
-# runner.run(150.)
-#
-# fig, gs = bp.visualize.get_figure(2, 1, 3, 8)
-# fig.add_subplot(gs[0, 0])
-# plt.plot(runner.mon.ts, runner.mon['pre.V'], label='pre-V')
-# plt.plot(runner.mon.ts, runner.mon['post.V'], label='post-V')
-# plt.legend()
-#
-# fig.add_subplot(gs[1, 0])
-# plt.plot(runner.mon.ts, runner.mon['syn.g'], label='g')
-# plt.legend()
-# plt.show()
+	# 运行模拟
+	runner = bp.dyn.DSRunner(net, inputs=[('pre.input', 5.)], monitors=['pre.V', 'post.V', 'syn.g'])
+	runner.run(run_duration)
+
+	fig, gs = plt.subplots(2, 1, figsize=(6, 4.5))
+	plt.sca(gs[0])
+	plt.plot(runner.mon.ts, runner.mon['pre.V'], label='pre-V')
+	plt.plot(runner.mon.ts, runner.mon['post.V'], label='post-V')
+	plt.legend(loc='upper right')
+
+	plt.sca(gs[1])
+	plt.plot(runner.mon.ts, runner.mon['syn.g'], label='g', color=u'#d62728')
+	plt.legend(loc='upper right')
+	plt.show()
