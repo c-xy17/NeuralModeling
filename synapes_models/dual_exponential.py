1be69eec2d4de40be294eda2ca4f4c1c0b18e2f7
c-xy17 <c-xy17@tsinghua.org.cn>
2022-04-09 17:18:23 +0800

    modify the Dual Synapse model

--- synapes_models/dual_exponential.py
+++ synapes_models/dual_exponential.py
@@ -5,7 +5,7 @@ from run_synapse import run_syn
 
 
 class DualExponential(bp.dyn.TwoEndConn):
-	def __init__(self, pre, post, conn, g_max=1., tau_decay=8., tau_rise=2., delay_step=2,
+	def __init__(self, pre, post, conn, g_max=0.6, tau_decay=8., tau_rise=2., delay_step=2,
 	             E=0., syn_type='CUBA', method='exp_auto', **kwargs):
 		super(DualExponential, self).__init__(pre=pre, post=post, conn=conn, **kwargs)
 		self.check_pre_attrs('spike')
@@ -15,7 +15,6 @@ class DualExponential(bp.dyn.TwoEndConn):
 		self.tau_decay = tau_decay
 		self.tau_rise = tau_rise
 		self.g_max = g_max
-		self.h_max = (tau_decay - tau_rise) / (tau_decay * tau_rise)
 		self.delay_step = delay_step
 		self.E = E
 
@@ -32,7 +31,7 @@ class DualExponential(bp.dyn.TwoEndConn):
 
 		# 定义微分方程及其对应的积分函数
 		self.int_h = bp.odeint(method=method, f=lambda h, t: -h / self.tau_rise)
-		self.int_g = bp.odeint(method=method, f=lambda g, t, h: -g / self.tau_decay + h * self.g_max)
+		self.int_g = bp.odeint(method=method, f=lambda g, t, h: -g / self.tau_decay + h)
 
 	def update(self, _t, _dt):
 		# 将突触前神经元传来的信号延迟delay_step的时间步长
@@ -40,7 +39,7 @@ class DualExponential(bp.dyn.TwoEndConn):
 		self.delay.update(self.pre.spike)
 
 		# 根据连接模式计算各个突触后神经元收到的信号强度
-		post_sp = bm.pre2post_event_sum(delayed_pre_spike, self.pre2post, self.post.num, self.h_max)
+		post_sp = bm.pre2post_event_sum(delayed_pre_spike, self.pre2post, self.post.num, self.g_max)
 		# 突触的电导g的更新包括常规积分和突触前脉冲带来的跃变
 		self.h.value = self.int_h(self.h, _t) + post_sp
 		self.g.value = self.int_g(self.g, _t, self.h)
@@ -52,5 +51,5 @@ class DualExponential(bp.dyn.TwoEndConn):
 			self.post.input += self.g * (self.E - self.post.V)
 
 
-run_syn(DualExponential, syn_type='CUBA', title='Dual Exponential Synapse Model (Current-Based)')
-run_syn(DualExponential, syn_type='COBA', title='Dual Exponential Synapse Model (Conductance-Based)')
+# run_syn(DualExponential, syn_type='CUBA', title='Dual Exponential Synapse Model (Current-Based)')
+# run_syn(DualExponential, syn_type='COBA', title='Dual Exponential Synapse Model (Conductance-Based)')
