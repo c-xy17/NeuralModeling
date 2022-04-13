e499f1f6f59206a881c2a4cee595511a015cca6c
c-xy17 <c-xy17@tsinghua.org.cn>
2022-04-13 17:14:12 +0800

    add the GABAa model

---
+++ synapes_models/GABA_A.py
@@ -0,0 +1,13 @@
+from AMPA import AMPA
+from run_synapse import run_syn
+
+
+class GABAa(AMPA):
+	def __init__(self, pre, post, conn, g_max=4., E=-80., alpha=0.53, beta=0.18,
+	             T_0=1., T_dur=1., delay_step=2, method='exp_auto', **kwargs):
+		super(GABAa, self).__init__(pre, post, conn, g_max=g_max, E=E, alpha=alpha,
+		                            beta=beta, T_0=T_0, T_dur=T_dur,
+		                            delay_step=delay_step, method=method, **kwargs)
+
+
+run_syn(GABAa, title='GABA$_\mathrm{A}$ Synapse Model')
