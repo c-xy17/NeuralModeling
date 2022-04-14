8b31688d78f289123c250a3e5c3f542805a3c264
c-xy17 <c-xy17@tsinghua.org.cn>
2022-04-14 18:55:03 +0800

    add the NMDA model

--- synapes_models/NMDA2.py
+++ synapes_models/NMDA2.py
@@ -1,21 +1,25 @@
 import brainpy as bp
 import brainpy.math as bm
 
-from run_synapse import run_syn
+from run_synapse import run_syn_NMDA
 
 
-class AMPA(bp.dyn.TwoEndConn):
-	def __init__(self, pre, post, conn, g_max=1., E=0., alpha=0.98, beta=0.18,
-	             T_0=0.5, T_dur=0.5, delay_step=2, method='exp_auto', **kwargs):
-		super(AMPA, self).__init__(pre=pre, post=post, conn=conn, **kwargs)
+class NMDA(bp.dyn.TwoEndConn):
+	def __init__(self, pre, post, conn, g_max=0.4, E=0., c_Mg=1.2, alpha1=2.,
+	             beta1=0.01, alpha2=1., beta2=0.5, T_0=1., T_dur=0.5, delay_step=2,
+	             method='exp_auto', **kwargs):
+		super(NMDA, self).__init__(pre=pre, post=post, conn=conn, **kwargs)
 		self.check_pre_attrs('spike')
 		self.check_post_attrs('input', 'V')
 
 		# 初始化参数
 		self.g_max = g_max
 		self.E = E
-		self.alpha = alpha
-		self.beta = beta
+		self.c_Mg = c_Mg
+		self.alpha1 = alpha1
+		self.beta1 = beta1
+		self.alpha2 = alpha2
+		self.beta2 = beta2
 		self.T_0 = T_0
 		self.T_dur = T_dur
 		self.delay_step = delay_step
@@ -24,17 +28,18 @@ class AMPA(bp.dyn.TwoEndConn):
 		self.pre2post = self.conn.require('pre2post')  # 获取从pre到post的连接信息
 
 		# 初始化变量
+		self.x = bm.Variable(bm.zeros(self.post.num))
 		self.s = bm.Variable(bm.zeros(self.post.num))
 		self.g = bm.Variable(bm.zeros(self.post.num))
+		self.b = bm.Variable(bm.zeros(self.post.num))
 		self.spike_arrival_time = bm.Variable(bm.ones(self.pre.num) * -1e7)  # 脉冲到达的时间
 		self.delay = bm.LengthDelay(self.pre.spike, delay_step)  # 定义一个延迟处理器
 
 		# 定义积分函数
-		self.integral = bp.odeint(self.derivative, method=method)
-
-	def derivative(self, s, t, T):
-		dsdt = self.alpha * T * (1 - s) - self.beta * s
-		return dsdt
+		self.int_s = bp.odeint(method=method,
+		                       f=lambda s, t, x: self.alpha1 * x * (1 - s) - self.beta1 * s)
+		self.int_x = bp.odeint(method=method,
+		                       f=lambda x, t, T: self.alpha2 * T * (1 - x) - self.beta2 * x)
 
 	def update(self, _t, _dt):
 		# 将突触前神经元传来的信号延迟delay_step的时间步长
@@ -45,12 +50,16 @@ class AMPA(bp.dyn.TwoEndConn):
 		self.spike_arrival_time.value = bm.where(delayed_pre_spike, _t, self.spike_arrival_time)
 		T = ((_t - self.spike_arrival_time) < self.T_dur) * self.T_0
 
-		# 更新s和g
-		self.s.value = self.integral(self.s, _t, T, dt=_dt)
+		# 更新x，s和g
+		self.x.value = self.int_x(self.x, _t, T)
+		self.s.value = self.int_s(self.s, _t, self.x)
 		self.g.value = self.g_max * self.s
 
+		# 更新b
+		self.b.value = 1 / (1 + bm.exp(-0.062 * self.post.V) * self.c_Mg / 3.57)
+
 		# 电导模式下计算突触后电流大小
-		self.post.input += self.g * (self.E - self.post.V)
+		self.post.input += self.g * self.b * (self.E - self.post.V)
 
 
-run_syn(AMPA, title='AMPA Synapse Model')
+run_syn_NMDA(NMDA, title='NMDA Synapse Model (Kinetic)')
--- synapes_models/run_synapse.py
+++ synapes_models/run_synapse.py
@@ -20,7 +20,7 @@ def run_syn_LIF(syn_model, run_duration=30., **kwargs):
 	plt.show()
 
 
-def run_syn(syn_model, title, run_duration=100., **kwargs):
+def run_syn(syn_model, title, run_duration=100., Iext=5., **kwargs):
 	# 定义突触前神经元、突触后神经元和突触连接，并构建神经网络
 	neu1 = bp.dyn.HH(1)
 	neu2 = bp.dyn.HH(1)
@@ -28,7 +28,7 @@ def run_syn(syn_model, title, run_duration=100., **kwargs):
 	net = bp.dyn.Network(pre=neu1, syn=syn1, post=neu2)
 
 	# 运行模拟
-	runner = bp.dyn.DSRunner(net, inputs=[('pre.input', 5.)], monitors=['pre.V', 'post.V', 'syn.g'])
+	runner = bp.dyn.DSRunner(net, inputs=[('pre.input', Iext)], monitors=['pre.V', 'post.V', 'syn.g'])
 	runner.run(run_duration)
 
 	# 可视化
@@ -41,6 +41,36 @@ def run_syn(syn_model, title, run_duration=100., **kwargs):
 
 	plt.sca(gs[1])
 	plt.plot(runner.mon.ts, runner.mon['syn.g'], label='g', color=u'#d62728')
+	plt.legend(loc='upper right')
+
+	plt.tight_layout()
+	plt.show()
+
+
+def run_syn_NMDA(syn_model, title, run_duration=100., Iext=5., **kwargs):
+	# 定义突触前神经元、突触后神经元和突触连接，并构建神经网络
+	neu1 = bp.dyn.HH(1)
+	neu2 = bp.dyn.HH(1)
+	syn1 = syn_model(neu1, neu2, conn=bp.connect.All2All(), **kwargs)
+	net = bp.dyn.Network(pre=neu1, syn=syn1, post=neu2)
+
+	# 运行模拟
+	runner = bp.dyn.DSRunner(net,
+	                         inputs=[('pre.input', Iext)],
+	                         monitors=['pre.V', 'post.V', 'syn.s', 'syn.b'])
+	runner.run(run_duration)
+
+	# 可视化
+	fig, gs = plt.subplots(2, 1, figsize=(6, 4.5))
+	plt.sca(gs[0])
+	plt.plot(runner.mon.ts, runner.mon['pre.V'], label='pre-V')
+	plt.plot(runner.mon.ts, runner.mon['post.V'], label='post-V')
+	plt.legend(loc='upper right')
+	plt.title(title)
+
+	plt.sca(gs[1])
+	plt.plot(runner.mon.ts, runner.mon['syn.s'], label='s', color=u'#d62728')
+	plt.plot(runner.mon.ts, runner.mon['syn.b'], label='b', color=u'#2ca02c')
 	plt.legend(loc='upper right')
 
 	plt.tight_layout()
