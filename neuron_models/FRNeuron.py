69df264ad3e6a4f502d38fd4b016385cdcdaa00f
Xiaoyu Chen <c-xy17@tsinghua.org.cn>
2022-04-24 23:35:13 +0800

    add a simple firing-rate model

---
+++ neuron_models/FRNeuron.py
@@ -0,0 +1,12 @@
+import brainpy as bp
+import brainpy.math as bm
+
+class FR(bp.dyn.NeuGroup):
+  def __init__(self, size, **kwargs):
+    super(FR, self).__init__(size=size, **kwargs)
+    self.r = bm.Variable(bm.zeros(self.num))
+    self.input = bm.Variable(bm.zeros(self.num))
+
+  def update(self, _t, _dt):
+    self.r.value = self.input  # 将输入直接视为r
+    self.input[:] = 0.
