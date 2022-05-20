b5cbd5e7c84dfb5b2ddde1dc8fde316627f8736b
Xiaoyu Chen <55552143+c-xy17@users.noreply.github.com>
2022-05-20 18:35:16 +0800

    Update the EI balance model

--- network_analysis/EI_balance_analysis.py
+++ network_analysis/EI_balance_analysis.py
@@ -9,40 +9,54 @@ from network_models.EI_balance import EINet
 # 数值模拟
 net = EINet(3200, 800)
 runner = bp.DSRunner(net,
-                         monitors=['E.spike', 'I.spike', 'E.input', 'I.input'],
-                         inputs=[('E.input', 20.), ('I.input', 20.)])
+                     monitors=['E.spike', 'I.spike', 'E.input', 'E.V'],
+                     inputs=[('E.input', 12.), ('I.input', 12.)])
 runner(200.)
 
 # 可视化
-def raster_plot(spikes, title):  # 可视化脉冲发放
+# 定义可视化脉冲发放的函数
+def raster_plot(spikes, title):
   t, neu_index = np.where(spikes)
   t = t * bp.math.get_dt()
   plt.scatter(t, neu_index, s=0.5, c='k')
   plt.title(title)
   plt.ylabel('neuron index')
 
-def fr_plot(t, spikes):  # 可视化平均发放速率
+# 定义可视化平均发放速率的函数
+def fr_plot(t, spikes):
   rate = bp.measure.firing_rate(spikes, 5.)
   plt.plot(t, rate)
   plt.ylabel('firing rate')
   plt.xlabel('t (ms)')
 
-fig, gs = plt.subplots(2, 2, gridspec_kw={'height_ratios': [3, 1]}, figsize=(12, 8), sharex='all')
-plt.sca(gs[0, 0])
-raster_plot(runner.mon['E.spike'], 'Spikes of Excitatory Neurons')
-plt.sca(gs[0, 1])
-raster_plot(runner.mon['I.spike'], 'Spikes of Inhibitory Neurons')
+# # 可视化脉冲发放
+# fig, gs = plt.subplots(2, 2, gridspec_kw={'height_ratios': [3, 1]}, figsize=(12, 8), sharex='all')
+# plt.sca(gs[0, 0])
+# raster_plot(runner.mon['E.spike'], 'Spikes of Excitatory Neurons')
+# plt.sca(gs[0, 1])
+# raster_plot(runner.mon['I.spike'], 'Spikes of Inhibitory Neurons')
+#
+# # 可视化平均发放速率
+# plt.sca(gs[1, 0])
+# fr_plot(runner.mon.ts, runner.mon['E.spike'])
+# plt.sca(gs[1, 1])
+# fr_plot(runner.mon.ts, runner.mon['I.spike'])
+#
+# plt.subplots_adjust(hspace=0.1)
+# plt.show()
 
-plt.sca(gs[1, 0])
-fr_plot(runner.mon.ts, runner.mon['E.spike'])
-plt.sca(gs[1, 1])
-fr_plot(runner.mon.ts, runner.mon['I.spike'])
 
-plt.subplots_adjust(hspace=0.1)
+fig, gs = plt.subplots(2, 1, figsize=(6, 4), sharex='all')
+
+i = 299  # 随机指定一个神经元序号
+print(runner.mon['E.spike'][:, i].sum())
+gs[0].plot(runner.mon.ts, runner.mon['E.input'][:, i] + 12.)  # input中不包括外部输入，在此需加上12
+gs[0].set_ylabel('input current')
+
+gs[1].plot(runner.mon.ts, runner.mon['E.V'][:, i])
+gs[1].plot([0., 200.], [net.E.V_th, net.E.V_th], '--')
+gs[1].set_ylabel('V')
+
+plt.xlabel('t (ms)')
 plt.show()
 
-# i = 400
-# plt.plot(runner.mon.ts, runner.mon['E.input'][:, i])
-# plt.plot(runner.mon.ts, runner.mon['I.input'][:, i])
-# plt.plot(runner.mon.ts, runner.mon['E.input'][:, i] - runner.mon['I.input'][:, i])
-# plt.show()
--- network_models/EI_balance.py
+++ network_models/EI_balance.py
@@ -1,9 +1,7 @@
 import brainpy as bp
 import brainpy.math as bm
-import numpy as np
-import matplotlib.pyplot as plt
 
-
+# 调用BrainPy中的预置模型
 LIF = bp.dyn.LIF
 ExpCOBA = bp.dyn.ExpCOBA
 
@@ -13,7 +11,7 @@ class EINet(bp.dyn.Network):
     super(EINet, self).__init__(**kwargs)
 
     # 搭建神经元
-    pars = dict(V_rest=-60., V_th=-50., V_reset=-60., tau=20., tau_ref=5.)
+    pars = dict(V_rest=-60., V_th=-50., V_reset=-60., tau=20., tau_ref=5.)  # 神经元模型需要的参数
     E = LIF(num_exc, **pars, method=method)
     I = LIF(num_inh, **pars, method=method)
     E.V.value = bm.random.randn(num_exc) * 4. - 60.  # 随机初始化膜电位
@@ -22,8 +20,8 @@ class EINet(bp.dyn.Network):
     self.I = I
 
     # 搭建神经元连接
-    E_pars = dict(E=0., g_max=0.6, tau=5.)
-    I_pars = dict(E=-80., g_max=6.7, tau=10.)
+    E_pars = dict(E=0., g_max=0.3, tau=5.)  # 兴奋性突触需要的参数
+    I_pars = dict(E=-80., g_max=3.2, tau=10.)  # 抑制性突触需要的参数
     self.E2E = ExpCOBA(E, E, bp.conn.FixedProb(prob=0.02), **E_pars, method=method)
     self.E2I = ExpCOBA(E, I, bp.conn.FixedProb(prob=0.02), **E_pars, method=method)
     self.I2E = ExpCOBA(I, E, bp.conn.FixedProb(prob=0.02), **I_pars, method=method)
